0d42e8612254539c4ad91835d7f58cda25ebabfa
Skasix00 <brunoalexcastelo@gmail.com>
2022-07-24 00:14:43 +0100

    Testing stuff

--- islands/Countdown.tsx
+++ islands/Countdown.tsx
@@ -33,6 +33,6 @@ export default function Countdown(props: { target: string }) {
 
   // Otherwise, we format the remaining time using `Intl.RelativeTimeFormat` and
   // render it.
-  const secondsLeft = Math.floor((target.getTime() - now.getTime()) / 1000);
+  const secondsLeft = 15;
   return <span>{timeFmt.format(secondsLeft, "seconds")}</span>;
 }
